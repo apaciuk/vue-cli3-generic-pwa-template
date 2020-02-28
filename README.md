f64e8ff1ee1ddbc5879494b6fdc8ea5a4f83560c
Paul Anthony McGowan <xhostcom@gmail.com>
2020-02-28 21:55:09 +0000

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# vue-cli3-generic-pwa-template
-Vue  PWA Template for CLI 3 with extra files/plugins ready to go
+# vue-cli3-generic-pwa
+
+Generic Vue CLI PWA Template with PWA plugin added, other npm packages, manifest.json and registerServiceWorker ready to go
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ dist/precache-manifest.369ec8382e1dda5cb4ed15df1b8a170f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91cceb939ab7b659f1b",
+    "url": "/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "817ac6a0cfcc4d1ea7e0f7edeeed256a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b91cceb939ab7b659f1b",
+    "url": "/js/app.8030e221.js"
+  },
+  {
+    "revision": "cb1509690a96e4f08bc4",
+    "url": "/js/chunk-vendors.6c794a53.js"
+  },
+  {
+    "revision": "f21410a10eb70a196e9db138573d394f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
