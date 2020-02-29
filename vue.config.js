66f4b27a82455070f15f5932708049ebe232450c
Paul Anthony McGowan <xhostcom@gmail.com>
2020-02-29 08:43:35 +0000

    Update

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+//Use Example//
+
+/*var webpack = require('webpack');
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+      })
+    ]
+  }
+}*/
